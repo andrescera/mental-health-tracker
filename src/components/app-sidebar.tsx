d8254096a8a6d7ffb431a6571839fc5375eab4da
Andres Cera <andres.cera@hotmail.com>
2025-03-13 13:26:39 -0500

    build dashboard layout with navigation

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import * as React from "react";
+import { companyInfo, navigation } from "~/lib/contants";
+import { NavMain } from "~/components/nav-main";
+import { NavUser } from "~/components/nav-user";
+import { CompanyLogo } from "~/components/company-logo";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "~/components/ui/sidebar";
+import { useSession } from "next-auth/react";
+
+// This is sample data.
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { data: session } = useSession();
+  const user = {
+    name: session?.user?.name,
+    email: session?.user?.email,
+    avatar: session?.user?.image,
+  };
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <CompanyLogo companyInfo={companyInfo} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain menuTitle="Options" items={navigation} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
---
+++ src/lib/contants.ts
@@ -0,0 +1,48 @@
+import { Bot, Brain, ChartColumnDecreasing, Logs } from "lucide-react";
+
+export const companyInfo = {
+  name: "Mental Health Tracker",
+  logo: Brain,
+  plan: "Daily Tracker",
+};
+
+export const navigation = [
+  {
+    title: "Tracker",
+    url: "tracker",
+    icon: Logs,
+    isActive: true,
+    items: [
+      {
+        title: "Daily Tracker",
+        url: "daily-tracker",
+      },
+      {
+        title: "History",
+        url: "history",
+      },
+    ],
+  },
+  {
+    title: "Statistics",
+    url: "statistics",
+    icon: ChartColumnDecreasing,
+    items: [
+      {
+        title: "Global",
+        url: "global",
+      },
+    ],
+  },
+  {
+    title: "Recomendations",
+    url: "#",
+    icon: Bot,
+    items: [
+      {
+        title: "AI Recomendations",
+        url: "#",
+      },
+    ],
+  },
+];
